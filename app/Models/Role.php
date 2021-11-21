8c1f9903bea60338c3559c489869dba86acfc65a
level237 <level237>
2021-11-21 14:17:28 +0100

    adding the RoleController Api

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    private $table="roles";
+
+    private $fillable=[
+        'id',
+        'name',
+        'display_name'
+    ];
 }
