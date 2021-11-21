57c840ce50429a714c5e76b2b694e9460d9b6260
level237 <level237>
2021-11-21 13:56:00 +0100

    delete doctor and nurses' model

--- database/migrations/2021_11_21_123913_create_roles_table.php
+++ database/migrations/2021_11_21_123913_create_roles_table.php
@@ -15,6 +15,8 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('display_name');
             $table->timestamps();
         });
     }
