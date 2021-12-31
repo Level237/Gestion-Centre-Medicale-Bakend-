4d0a683844995d609df876fc30d7a1d5cae7f9e2
level237 <level237>
2021-12-31 19:23:49 +0100

    List of patients who had a prescription with a doctor

--- app/Http/Controllers/OrdonanceController.php
+++ app/Http/Controllers/OrdonanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Ordonance;
+use DB;
 
 class OrdonanceController extends Controller
 {
@@ -15,8 +16,13 @@ class OrdonanceController extends Controller
      */
     public function index()
     {
-        //
-       return  Ordonance::all();
+        $ordonance= DB::table('ordonances')
+		->join('patients','patients.id','=','ordonances.patient_id')
+		->join('personnels','personnels.id','=','ordonances.personnel_id')
+		->select('ordonances.id as id','ordonances.type as type','ordonances.composition as composition','patients.name as  patient_name','patients.id as patient_id','personnels.nom as personnel_name','personnels.specialisation','personnels.id as personnel_id')
+		->get();
+		return $ordonance;
+      
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PersonnelController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\ConsultationController;
+use App\Http\Controllers\OrdonanceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,7 +27,7 @@ Route::apiResource("/room",RoomController::class);
 Route::apiResource("/appointment",AppointmentController::class);
 Route::apiResource("/consultation",ConsultationController::class);
 Route::get('/numberOfAppointment','App\Http\Controllers\PersonnelController@getNumberOfAppointment');
-
+Route::apiResource("/ordonance",OrdonanceController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
